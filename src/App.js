d672dd9f388be2ff218858e68a12a9b0017365f9
Denny Azevedo <denny_af@yahoo.com.br>
2020-09-24 15:30:56 -0300

    Primeiras codificações

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => { 
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            {item.title}
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
-
-export default App;
